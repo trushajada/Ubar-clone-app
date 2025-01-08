a85bbf45029a2e68becdad8e513187cb1491a674
Your Name <you@example.com>
2025-01-09 05:11:45 +0530

    add

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,13 @@
-const http =require('http');
-const app =require('./app');
-const port =process.env.PORT ||3000;
 
-const server =http.createServer(app);
+const http = require('http');
+const app = require('./app');
+// const { initializeSocket } = require('./socket');
+const port = process.env.PORT || 3000;
 
-server.listen(port,()=>{
-    console.log(`server running ${port}`);
-    
+const server = http.createServer(app);
+
+// initializeSocket(server);
+
+server.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
 });
