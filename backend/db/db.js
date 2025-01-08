e233ae2705f7a8078e8a9cc70322813a6f6fdd39
Your Name <you@example.com>
2025-01-09 01:19:50 +0530

    add code

--- backend/db/db.js
+++ backend/db/db.js
@@ -3,8 +3,7 @@ const mongoose = require('mongoose');
 async function connectToDb() {
    mongoose.connect(process.env.DB_CONNECT).then(()=>{
         console.log("connect DB");
-        
-    }).catch(err=>console.log(err)
+         }).catch(err=>console.log(err)
     )
 }
 
