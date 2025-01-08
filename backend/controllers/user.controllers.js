bd9fb65b6abf00f7459a3d238690a73726e516ed
Your Name <you@example.com>
2025-01-09 04:05:36 +0530

    add code

--- backend/controllers/user.controllers.js
+++ backend/controllers/user.controllers.js
@@ -1,1 +1,12 @@
+const { validationResult } = require('express-validator')
 const userModel =require('../models/user.model')
+const userServices =require('../services/user.services ')
+
+
+model.exports.registerUser =async (req , res ,next)=>{
+
+    const error =validationResult(req);
+    if(!error.isEmpty()){
+        return res.status(400).json({error:error.array()})
+    }
+}
