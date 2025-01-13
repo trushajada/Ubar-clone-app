93853f7bf5e92c4aba1b96d28c4dde705e2e0626
Your Name <you@example.com>
2025-01-14 00:23:54 +0530

    add code

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -38,7 +38,7 @@ userSchema.methods.comparePassword = async function (password) {
     return await bcrypt.compare(password, this.password);
 }
 
-userSchema.statics.hashPassword = async function (password) {
+userSchema.statics.hashPassword = async function (password){
     return await bcrypt.hash(password, 10);
 }
 
