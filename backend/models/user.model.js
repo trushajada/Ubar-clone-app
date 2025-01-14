62fc317fa3ff8130b65bf61264afdc65f8bff64b
Your Name <you@example.com>
2025-01-15 02:42:32 +0530

    add code

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -2,7 +2,6 @@ const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 
-
 const userSchema = new mongoose.Schema({
     fullname: {
         firstname: {
@@ -37,12 +36,8 @@ userSchema.methods.generateAuthToken = function () {
 userSchema.methods.comparePassword = async function (password) {
     return await bcrypt.compare(password, this.password);
 }
-
 userSchema.statics.hashPassword = async function (password){
     return await bcrypt.hash(password, 10);
 }
-
 const userModel = mongoose.model('user', userSchema);
-
-
 module.exports = userModel;
