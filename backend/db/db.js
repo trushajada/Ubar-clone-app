7f4434dba07fc13418c5a9ada76a915c1ac5f1ea
Your Name <you@example.com>
2025-01-15 05:33:53 +0530

    add cpde

--- backend/db/db.js
+++ backend/db/db.js
@@ -1,12 +1,12 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://127.0.0.1:27017/ubar-clone-app') 
+mongoose.connect('mongodb://0.0.0.0/ubar-clone-app') 
   .then(() => console.log('MongoDB Connected!'))
   .catch(err => console.error('MongoDB Connection Error:', err));
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI || 'mongodb://127.0.0.1:27017/ubar-clone-app', {
+    const conn = await mongoose.connect(process.env.MONGO_URI || 'mongodb://0.0.0.0/ubar-clone-app', {
     });
 
     console.log(`MongoDB Connected:`);
