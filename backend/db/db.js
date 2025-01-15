d5718a1b6f97aea69c92f285a2019a8ffbdbf84e
Your Name <you@example.com>
2025-01-15 05:32:32 +0530

    add code

--- backend/db/db.js
+++ backend/db/db.js
@@ -1,9 +1,19 @@
 const mongoose = require('mongoose');
 
-const connectDB = async () => {
-    mongoose.connect('mongodb://127.0.0.1:27017/ubar-clone-app') 
+mongoose.connect('mongodb://127.0.0.1:27017/ubar-clone-app') 
   .then(() => console.log('MongoDB Connected!'))
   .catch(err => console.error('MongoDB Connection Error:', err));
+
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URI || 'mongodb://127.0.0.1:27017/ubar-clone-app', {
+    });
+
+    console.log(`MongoDB Connected:`);
+  } catch (error) {
+    console.error(`Error:`);
+    process.exit(1);
+  }
 }
 
-module.exports = connectDB;
+module.exports = connectDB
