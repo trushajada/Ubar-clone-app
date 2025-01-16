68588775325f5240d742fbf3a9a58d3f246a7e56
Your Name <you@example.com>
2025-01-17 00:29:05 +0530

    add code

--- backend/db/db.js
+++ backend/db/db.js
@@ -2,32 +2,15 @@ require('dotenv').config();
 const mongoose = require('mongoose');
 
 const connectToDb = async () => {
-  try {
-    await mongoose.connect(process.env.DB_CONNECT, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      // serverSelectionTimeoutMS: 5000,
-      // socketTimeoutMS: 45000,
-    });
-    
-    mongoose.connection.on('connected', () => {
-      console.log('MongoDB Connected successfully');
-    });
+  const mongoose = require('mongoose');
 
-    mongoose.connection.on('error', (err) => {
-      console.error('MongoDB connection error:', err);
-    });
-
-    mongoose.connection.on('disconnected', () => {
-      console.log('MongoDB disconnected');
-    });
-
-  } catch (error) {
-    console.error('Failed to connect to MongoDB:', error);
-    process.exit(1);
-  }
-};
-
+mongoose.connect('mongodb+srv://trushajada:2ySXU3VDWjlHXeHb@cluster0.v5bkx.mongodb.net/Uber_Clone', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
+.then(() => console.log('Connected to MongoDB'))
+.catch(err => console.error('Connection error:', err));
+}
 module.exports = connectToDb;
 
 
