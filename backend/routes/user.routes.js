18773e798dbfa038b21813e0ea72698bd1b0a39b
Your Name <you@example.com>
2025-01-31 21:30:39 +0530

    DD CODE

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -4,14 +4,12 @@ const { body } = require('express-validator');
 const userController = require('../controller/user.controller');
 const authMiddleware = require('../middalware/auth.middalware');
 
-// Register user route
 router.post('/register', [
     body('email').isEmail().withMessage('Please enter a valid email'),
     body('fullname.firstname').isLength({ min: 3 }).withMessage('First name must be at least 3 characters long'),
     body('fullname.lastname').isLength({ min: 3 }).withMessage('Last name must be at least 3 characters long'),
     body('password').isLength({ min: 6 }).withMessage('Password must be at least 6 characters long')
 ], userController.registerUser);
-
 
 router.post('/login', [
     body('email').isEmail().withMessage('please enter valid email'),
